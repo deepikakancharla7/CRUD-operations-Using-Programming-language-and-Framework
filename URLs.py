ccedacab02f17ae9731fc51f50900afbec19cff6
deepikakancharla7 <151307563+deepikakancharla7@users.noreply.github.com>
2024-02-08 11:00:42 +0530

    Delete URLs.py

--- URLs.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.registration_list, name='registration_list'),
-    path('new/', views.registration_create, name='registration_new'),
-    path('edit/<int:pk>/', views.registration_update, name='registration_edit'),
-    path('delete/<int:pk>/', views.registration_delete, name='registration_delete'),
-]
